fa8146b2416987826ffaa2537990b6e0a157a13c
sohana <sshabnamzsf@gmail.com>
2023-06-15 01:03:31 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import simage from '../assets/pic.png';
+
+function Home () {
+  return (
+    <div>
+    <div className="write">
+        Salon Ease India's top salon
+    </div>
+
+    <div class="container text-center">
+
+  <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
+
+    <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+    <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+     <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+     <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+     <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+    <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+    <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin: '25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+ <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin:'25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+ <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin:'25px' }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+    <div className="rec">
+    <img src={simage} alt="" style={{ borderRadius: '10%', height:'120px', margin:'25px'  }}  />
+    <div class="col">
+      <div>Row column</div>
+    </div>
+    </div>
+
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
