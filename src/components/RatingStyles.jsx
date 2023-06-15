206dcd75b20af6dfe93f5528bf583e4220222307
sohana <sshabnamzsf@gmail.com>
2023-06-15 16:27:58 +0530

    responsive

--- src/components/RatingStyles.jsx
+++ src/components/RatingStyles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
  
 export const Container = styled.div`
    display: flex;
@@ -6,10 +6,24 @@ export const Container = styled.div`
    align-items: center;
    min-height: 1px;
    font-size: 20px;
+
+   @media (max-width: 480px) {
+    flex-direction: column;
+    font-size: 16px;
+  }
+
 `
 export const Radio = styled.input`
    display: none;
 `
 export const Rating = styled.div`
    cursor: pointer;
+
+   ${({ responsive }) =>
+    responsive &&
+    css`
+      @media (max-width: 480px) {
+        font-size: 14px;
+      }
+    `}
 `
